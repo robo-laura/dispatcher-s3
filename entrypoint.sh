1fae0445d1f92c20f9620a67896b38a9c1708fb7
a-sync <a-sync@users.noreply.github.com>
2021-05-20 23:40:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-# if [ -z "$S3_BUCKET" ]; then
-#   echo "S3_BUCKET is not set. Quitting."
-#   exit 1
-# fi
-# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-#   exit 1
-# fi
-# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-#   exit 1
-# fi
+if [ -z "$S3_BUCKET" ]; then
+  echo "S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
 
-# if [-z "$FILE"]; then
-#   echo "FILE is not set. Quitting"
-#   exit 1
-# fi
+if [-z "$FILE"]; then
+  echo "FILE is not set. Quitting"
+  exit 1
+fi
 
-# if [ -z "$AWS_REGION"]; then
-#   AWS_REGION="us-east-1"
-# fi
+if [ -z "$AWS_REGION"]; then
+  AWS_REGION="eu-central-1"
+fi
 
 mkdir -p ~/.aws
 
@@ -32,9 +32,7 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws --version
-
-aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} --region ${AWS_REGION} --recursive $*
+aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
 
